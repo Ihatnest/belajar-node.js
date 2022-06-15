4ee299549a284fbdd160474a812acadbd3ec954a
Ihatnest <85156361+Ihatnest@users.noreply.github.com>
2022-06-15 11:11:04 +0700

    yy

--- module/script1.js
+++ module/script1.js
@@ -7,6 +7,7 @@
 // cara 3
 // const moment = require('moment'); // third-party module / npm module / node module
 
+
 const data = require('./script.js');
 
 console.log(`hai nama saya ${data.nama} dan kelas saya ${data.kelas} ini teman saya nama nya ${data.hallo('saya',12)}`);
