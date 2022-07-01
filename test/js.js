bfe363616301df6092365564fe5f561b7f759418
Ihatnest <85156361+Ihatnest@users.noreply.github.com>
2022-07-01 12:48:20 +0700

    t

--- test/js.js
+++ test/js.js
@@ -4,19 +4,20 @@ const secema = { name: String, }
 const Cat = mongoose.model('data', secema);
 // const arr = await Cat.find();
 
-const tets =
+const tets = () => {
   // return d
-  Cat.find({ name: 'Zildjianss' }, function (err, docs) {
+  Cat.findOne({ name: 'Zildjianss' }, function (err, docs) {
     if (err) {
       console.log(err)
     } else {
-      return docs
+      let d = docs
+      return d
     }
     // return docs
-  });
-
-
+  })
+}
+  
 // const kitty = new Cat({ name: 'Zildjianss' });
 // .save().then(() => console.log('meow'));
-console.log(tets)
+console.log(tets())
 
